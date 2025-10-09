51b813473a63bc2b65ebab4b062484ea4aaf0d80
Kian Janloo <ddrdslayer@gmail.com>
2025-10-09 13:57:01 +0330

    test del

--- src/gql-auth-guard/gql-auth.spec.ts
+++
@@ -1,7 +0,0 @@
-import { GqlAuthGuard } from './gql-auth.guard';
-
-describe('GqlAuthGuardGuard', () => {
-  it('should be defined', () => {
-    expect(new GqlAuthGuard()).toBeDefined();
-  });
-});
--- src/guns/guns.module.ts
+++ src/guns/guns.module.ts
@@ -3,9 +3,10 @@ import { GunsService } from './guns.service';
 import { GunsResolver } from './guns.resolver';
 import { Gun } from './entities/gun.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Gun])],
+  imports: [TypeOrmModule.forFeature([Gun]), AuthModule],
   providers: [GunsResolver, GunsService],
   exports: [GunsService],
 })
